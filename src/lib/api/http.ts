ecc929ff6b10b62fcac0e61b5d70bddc7ac0d8e9
moonkanghyeon <s25024@gsm.hs.kr>
2026-02-25 15:54:24 +0900

    fix: lint 오류 수정

--- src/lib/api/http.ts
+++ src/lib/api/http.ts
@@ -1,36 +1,26 @@
 import { axiosInstance } from './axiosInstance';
 
-export const get = async <T>(
-  ...args: Parameters<typeof axiosInstance.get>
-) => {
+export const get = async <T>(...args: Parameters<typeof axiosInstance.get>) => {
   const response = await axiosInstance.get<T>(...args);
   return response.data;
 };
 
-export const post = async <T>(
-  ...args: Parameters<typeof axiosInstance.post>
-) => {
+export const post = async <T>(...args: Parameters<typeof axiosInstance.post>) => {
   const response = await axiosInstance.post<T>(...args);
   return response.data;
 };
 
-export const put = async <T>(
-  ...args: Parameters<typeof axiosInstance.put>
-) => {
+export const put = async <T>(...args: Parameters<typeof axiosInstance.put>) => {
   const response = await axiosInstance.put<T>(...args);
   return response.data;
 };
 
-export const patch = async <T>(
-  ...args: Parameters<typeof axiosInstance.patch>
-) => {
+export const patch = async <T>(...args: Parameters<typeof axiosInstance.patch>) => {
   const response = await axiosInstance.patch<T>(...args);
   return response.data;
 };
 
-export const del = async <T>(
-  ...args: Parameters<typeof axiosInstance.delete>
-) => {
+export const del = async <T>(...args: Parameters<typeof axiosInstance.delete>) => {
   const response = await axiosInstance.delete<T>(...args);
   return response.data;
 };
