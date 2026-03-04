03e7ee0c4bd1e068b6fd9b1564cba011dca5a065
moonkanghyeon <s25024@gsm.hs.kr>
2026-03-04 10:05:29 +0900

    feat: XButton svg 생성

---
+++ src/assets/svg/XButton.tsx
@@ -0,0 +1,20 @@
+export default function XButton() {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_237_4362)">
+        <path
+          d="M18.5716 1.42847L1.42871 18.5713M1.42871 1.42847L18.5716 18.5713"
+          stroke="#536478"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_237_4362">
+          <rect width="20" height="20" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
