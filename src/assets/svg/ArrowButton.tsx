7f499b8a0b357fd6ed968e052e6086184bb73872
moonkanghyeon <s25024@gsm.hs.kr>
2026-03-04 10:05:14 +0900

    feat: ArrowButton 생성

---
+++ src/assets/svg/ArrowButton.tsx
@@ -0,0 +1,16 @@
+export default function ArrowButton() {
+  return (
+    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M9.5 19C14.7467 19 19 14.7467 19 9.5C19 4.25329 14.7467 0 9.5 0C4.25329 0 0 4.25329 0 9.5C0 14.7467 4.25329 19 9.5 19Z"
+        fill="#F5F5F5"
+      />
+      <path
+        d="M8.0752 13.7751L12.3502 9.5001L8.0752 5.2251"
+        stroke="#7E8D9E"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
